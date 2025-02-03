b1c53faaec47894efe783bbcf9c4c3210d890d52
Anagho <anaghosamroyce@gmail.com>
2025-02-03 12:17:09 +0100

    updated responsiveness

--- frontend/src/pages/MarketPlace.jsx
+++ frontend/src/pages/MarketPlace.jsx
@@ -3,7 +3,7 @@ import AllProducts from "../components/products/AllProducts";
 
 function MarketPlace() {
   return (
-    <section className="container p-2 py-6 md:py-10 mx-auto">
+    <section className="container py-6 md:py-10 mx-auto">
       <h1 className="text-base sm:text-lg md:text-xl font-semibold mb-8 text-slate-800">
         Shop Fresh Farm Products
       </h1>
