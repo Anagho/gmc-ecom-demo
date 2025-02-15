afd630e852c1b0a80876cd09bdec73a2049b60e4
Anagho <anaghosamroyce@gmail.com>
2025-02-15 02:54:37 +0100

    added changes

--- backend/controllers/authController/logoutUser.js
+++ backend/controllers/authController/logoutUser.js
@@ -2,8 +2,8 @@
 export const logoutUser = (req, res) => {
    res.clearCookie("token", {
      httpOnly: true,
-     secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Required for cross-origin cookies
+     secure: process.env.NODE_ENV === "production",
+     sameSite: "strict", // prevents CSRF attacks
    });
   res.status(200).json({ success: true, message: "Logged out successfully" });
 };
