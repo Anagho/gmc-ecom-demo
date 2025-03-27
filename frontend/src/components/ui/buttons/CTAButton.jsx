3baf52833c41e8174c65fc7aec0e3ca595a20c42
Anagho <anaghosamroyce@gmail.com>
2025-03-27 16:13:02 +0100

    new changes

--- frontend/src/components/ui/buttons/CTAButton.jsx
+++ frontend/src/components/ui/buttons/CTAButton.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 function CTAButton({ buttonText }) {
   return (
     <motion.button
-      className="p-2 text-base md:text-lg bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+      className="p-2 text-sm sm:text-base md:text-lg bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.98 }}
       type="submit"
